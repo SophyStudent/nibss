17b3a4334df7295d9d37d7efc063ab0a3b6393e2
XBlankZGap <170510274+XBlankZGap@users.noreply.github.com>
2026-09-18 21:29:06 +0100

    auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,15 +1,35 @@
-const registerCustomer = (req, res) => {
+const {
+    registerCustomer: registerCustomerService,
+    loginCustomer: loginCustomerService
+} = require("../services/auth.service");
+
+const registerCustomer = async (req, res) => {
     const { name, email, password } = req.body;
 
+    const customer = await registerCustomerService({
+        name,
+        email,
+        password
+    });
+
     res.status(201).json({
-        message: "Customer registration received",
-        customer: {
-            name,
-            email
-        }
+        message: "Customer registered successfully",
+        customer
     });
 };
 
+const loginCustomer = async (req, res) => {
+    const { email, password } = req.body;
+
+    const result = await loginCustomerService({
+        email,
+        password
+    });
+
+    res.status(200).json(result);
+};
+
 module.exports = {
-    registerCustomer
+    registerCustomer,
+    loginCustomer
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,12 +5,22 @@ const {
 } = require("../controllers/auth.controller");
 
 const { validateRegistration } = require("../validators/auth.validator");
+const asyncHandler = require("../utils/asyncHandler");
+
 const router = express.Router();
 
 // POST /register → register a new customer
-router.post("/register", validateRegistration, registerCustomer); //validateRegistration middleware added for input validation
+router.post(
+    "/register",
+    validateRegistration,
+    asyncHandler(registerCustomer)
+); //validateRegistration middleware added for input validation
+// for every async controller that you want the centralized error handler to catch, we wrap it with asyncHandler.
 
 // POST /login → authenticate an existing customer
-router.post("/login", loginCustomer);
+router.post(
+    "/login",
+    asyncHandler(loginCustomer)
+);
 
 module.exports = router;
