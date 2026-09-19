0465b941fecf396464000e4f8b69fd1ac50b7f9d
SophyStudent <233377597+SophyStudent@users.noreply.github.com>
2026-09-19 10:32:48 +0100

    swagger

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
 const express = require("express");
 
 const authRoutes = require("./routes/auth.routes");
@@ -11,6 +13,12 @@ const errorMiddleware = require("./middleware/error.middleware");
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get("/api-docs.json", (req, res) => {
+    res.json(swaggerSpec);
+});
 
 // Register our API routes
 app.use("/api/auth", authRoutes);
