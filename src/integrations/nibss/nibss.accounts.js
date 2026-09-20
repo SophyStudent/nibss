164d90c9fb8802cc084232a7dde628b88abe7b07
SophyStudent <233377597+SophyStudent@users.noreply.github.com>
2026-09-20 15:55:11 +0100

    postdebug_cleaning

--- src/integrations/nibss/nibss.accounts.js
+++ src/integrations/nibss/nibss.accounts.js
@@ -4,39 +4,21 @@ const { getNibssToken } = require("./nibss.auth");
 const createNibssAccount = async (kycType, kycID, dob) => {
     const token = await getNibssToken();
 
-    console.log("NIBSS account request:", {
-        kycType,
-        kycID,
-        dob
-    });
-
-    try {
-        const response = await nibssClient.post(
-            "/api/account/create",
-            {
-                kycType: kycType.toLowerCase(),
-                kycID,
-                dob
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
+    const response = await nibssClient.post(
+        "/api/account/create",
+        {
+            kycType: kycType.toLowerCase(),
+            kycID,
+            dob
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
             }
-        );
+        }
+    );
 
-       console.log("NIBSS account response:", response.data);
-
-return response.data;
-
-    } catch (error) {
-        console.log(
-            "NIBSS account creation error:",
-            error.response?.data
-        );
-
-        throw error;
-    }
+    return response.data;
 };
 
 module.exports = {
