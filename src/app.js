f77319b1f19cad863f9ad40f9f8cbee5b89d10d9
SophyStudent <233377597+SophyStudent@users.noreply.github.com>
2026-09-21 18:43:18 +0100

    app js

--- src/app.js
+++ src/app.js
@@ -28,12 +28,8 @@ app.use(express.json());
 
 app.use(
     "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(null, {
-        swaggerOptions: {
-            url: "/api-docs.json"
-        }
-    })
+    swaggerUi.serveFiles(swaggerSpec),
+    swaggerUi.setup(swaggerSpec)
 );
 
 app.get("/api-docs.json", (req, res) => {
